15c9d4827e92d4b768e0cc8f2c6aebcc8cafe958
gregbodet <37414914+gregbodet@users.noreply.github.com>
2024-10-14 11:42:41 +0200

    Update init_bd_docurba_v2024.sql

--- bdd/init_bd_docurba_v2024.sql
+++ bdd/init_bd_docurba_v2024.sql
@@ -1,1 +1,3 @@
+-- ############################################################################################ SUIVI CODE SQL ###################################################################################################
 
+-- 2024/10/14 : GB / Initialisation de la structure de la base de données pour le modèle CNIG des PLU, PLUI et CC version décembre 2024
