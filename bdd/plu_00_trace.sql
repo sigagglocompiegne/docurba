c2f31740ffe7c4166ebe095e2de4ab57c9046556
gregbodet <37414914+gregbodet@users.noreply.github.com>
2024-10-17 10:17:58 +0200

    Update plu_00_trace.sql

--- bdd/plu_00_trace.sql
+++ bdd/plu_00_trace.sql
@@ -41,5 +41,5 @@
 -- l'attribut NATURE a été complété par l'attribut L_NATURE en minuscule et les blancs remplacés par des '_' (du fait des recommandations de contenus du standard, mot séparé par le caractère '_' et non d'un espace)
 -- l'attribut L_NATURE reste un attribut métier à compléter pour un affichage utilisateur clair. L'attribut NATURE pour le standard est complété automatiquement à la saisie ou à la mise jour
 -- les valeurs des attributs NOMFIC et URLFIC ont été modifiées pour la classe d'objets des prescriptions surfaciques concernant les OAP : remplacements de 'orientations_aménagement' par oap dans le nom du fichiers et le répertoire '5_Orientations_amenagement' par '5_OAP'
-
+-- le script de migrations a été initialisé pour migrer le schéma 'm_urbanisme_doc' vers un schéma 'm_urbanisme_doc_v2024'
 
