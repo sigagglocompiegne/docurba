2cd5abe5927a848181b4c5ab1ea2efe6377a1e18
gregbodet <37414914+gregbodet@users.noreply.github.com>
2024-10-17 09:35:13 +0200

    Create plu_00_trace.sql

---
+++ bdd/plu_00_trace.sql
@@ -0,0 +1,15 @@
+/*PLU V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* plu_00_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+-- Le développement originel a débuté en 2015. Le développement a évolué et a été optimisé depuis sans qu'il n'y est de documentation.
+
+-- 2024-10-17 : GB / passage au standard PLU 2017d au stzandard 2024
+
+-- 
